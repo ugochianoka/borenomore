2357b6d59308d4ce8f0de5b462730de24d8551b3
ugochianoka <88979050+ugochianoka@users.noreply.github.com>
2024-09-11 02:12:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-/**
-Challenge: 
-
-- Make the styling more exciting once an activity idea comes 
-back from the Bored API
-    - Resources: DOM element "classList" property, uigradients.com, 
-      Google Fonts, color.adobe.com
-    - Some ideas:
-      - Change the title from "BoredBot" to something more exciting!
-      - Change the background to something less drab.
-      - Bonus: Animate something on the screen to move around and add more 
-        excitement to the page
-*/
 
 document.getElementById("get-activity").addEventListener("click", function() {
   fetch("https://apis.scrimba.com/bored/api/activity")
